aebc24f5ffbdef19c765813462f70035e5cbccfc
An Tran <dinhan2509@gmail.com>
2021-12-10 21:30:40 +0700

    Backend as a Service

---
+++ HTML_CLIENT/NVS.js
@@ -0,0 +1,140 @@
+const config = { databaseURL: "https://antran259820-default-rtdb.asia-southeast1.firebasedatabase.app/"}; 
+firebase.initializeApp (config); 
+const dbRef = firebase.database().ref(); 
+
+
+
+function page_Load(){
+    laydanhsach();
+}
+
+function btnSearch_Click(){
+    var keyword = document.getElementById("txtKeyword").value.trim();
+    if(keyword.length > 0){
+        search(keyword);
+    }else{
+        laydanhsach();
+    }
+}
+
+function lnkID_Click(Manv){
+    getDetails(Manv);
+}
+
+function btnAdd_Click(){
+    var newnhanvien = {
+        Manv: document.getElementById("txtManv").value,
+        Ten: document.getElementById("txtTen").value,
+        Chucvu: document.getElementById("txtChucvu").value,
+        Phongban: document.getElementById("txtPhongban").value,
+        Chuthich: document.getElementById("txtChuthich").value
+    };
+    addNew(newnhanvien);
+}
+
+function btnUpdate_Click(){
+    var newnhanvien = {
+        Manv:document.getElementById("txtManv").value,
+        Ten: document.getElementById("txtTen").value,
+        Chucvu: document.getElementById("txtChucvu").value,
+        Phongban: document.getElementById("txtPhongban").value,
+        Chuthich: document.getElementById("txtChuthich").value
+    };
+    update(newnhanvien);
+}
+
+function btnDelete_Click(){
+    if(confirm("ARE U SURE?")){
+        var manv = document.getElementById("txtManv").value;
+        deletee(manv);
+    }
+}
+
+function laydanhsach() { 
+    dbRef.child("nhanviens").on("value", (snapshot) => { 
+        var nhanviens = [];
+        snapshot.forEach((child) => { 
+        //alert(child.key); 
+        var nhanvien = child.val(); 
+        nhanviens.push (nhanvien); }); 
+        renderNVList (nhanviens); });     
+}
+
+function getDetails(manv){
+    dbRef.child("nhanviens").once ("value", (snapshot) => { 
+        snapshot.forEach((child) => { 
+        var nhanvien = child.val();
+         if (nhanvien. Manv == manv) { 
+        renderNVDetails (nhanvien); 
+        }
+    }); 
+    }); 
+        
+}
+
+function search(keyword){
+    dbRef.child("nhanviens").once ("value", (snapshot) => { 
+        var nhanviens = []; snapshot.forEach((child) => { 
+        var nhanvien = child.val(); if (nhanvien.Ten.toLowerCase().includes (keyword.toLowerCase())) { 
+        nhanviens.push (nhanvien);}
+        }); 
+        renderNVList (nhanviens); });
+}
+
+function addNew(newnhanvien){
+    dbRef.child("nhanviens/NV" + newnhanvien.Manv).set(newnhanvien); 
+}
+
+function update(newnhanvien){
+    dbRef.child("nhanviens").once ("value", (snapshot) => { 
+        snapshot.forEach((child) => { 
+        var nhanvien = child.val(); 
+        if (nhanvien.Manv == newnhanvien.Manv) { 
+        var key = child. key; 
+        dbRef.child("nhanviens").child(key).set(newnhanvien); 
+    }
+        }); }); 
+        
+}
+
+function deletee(manv){
+    dbRef.child("nhanviens").once ("value", (snapshot) => { 
+        snapshot.forEach((child) => { 
+        var nhanvien = child.val(); if (nhanvien.Manv == manv) { 
+        var key = child. key; dbRef.child ("nhanviens").child (key).remove();} 
+        }); 
+        }); 
+        
+}
+
+function renderNVList(nhanviens){
+    var rows = "";
+    for(var ChitietNV of nhanviens){
+        rows += "<tr onclick='lnkID_Click(" + ChitietNV.Manv + ")' style='cursor:pointer'>";
+        rows += "<td>" + ChitietNV.Manv + "</td>";
+        rows += "<td>" + ChitietNV.Ten + "</td>";
+        rows += "<td>" + ChitietNV.Chucvu + "</td>";
+        rows += "<td>" + ChitietNV.Phongban + "</td>";
+        rows += "<td>" + ChitietNV.Chuthich + "</td>";
+        rows += "</td>";
+    }
+    var header = "<tr><th>Manv</th><th>Ten</th><th>Chucvu</th><th>Phongban</th><th>Chuthich</th></tr>";
+    document.getElementById("lstNVS").innerHTML = header + rows;
+}
+
+function renderNVDetails(nhanvien){
+    document.getElementById("txtManv").value = nhanvien.Manv;
+    document.getElementById("txtTen").value = nhanvien.Ten ;
+    document.getElementById("txtChucvu").value = nhanvien.Chucvu;
+    document.getElementById("txtPhongban").value = nhanvien.Phongban;
+    document.getElementById("txtChuthich").value = nhanvien.Chuthich;
+}
+
+function clearTextboxes(){
+    document.getElementById("txtManv").value ='';
+    document.getElementById("txtTen").value ='';
+    document.getElementById("txtChucvu").value ='';
+    document.getElementById("txtPhongban").value ='';
+    document.getElementById("txtChuthich").value ='';
+}
+
